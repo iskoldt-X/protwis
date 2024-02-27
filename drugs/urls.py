b741f9c619a8b60d36cf54eec3a6e72261d7c6d0
Jimmy-A-Caroli <jimmycaroli88@gmail.com>
2024-02-27 15:28:24 +0100

    UPDATE: setting up Sankey Plot for indication page

--- drugs/urls.py
+++ drugs/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^drugmapping',  views.drugmapping, name='drugmapping'),
     url(r'^nhs/section/(?P<slug>[\w|\W]+)/$',  views.nhs_section, name='nhs_section'),
     url(r'^nhs/(?P<slug>[\w|\W]+)/$',  views.nhs_drug, name='nhs_drug'),
+    url(r'^indication/(?P<code>[\w|\W]+)/$',  views.indication_detail, name='indication'),
+
 ]
